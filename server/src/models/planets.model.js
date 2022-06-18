16c155bef0ff1a1e8b89d5d50b4cfb1973091137
Max Ohsawa <max.ohsawa@gmail.com>
2022-06-17 20:40:02 -0400

    handles async planet loading

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,5 +1,6 @@
-const parse = require('csv-parse');
-const fs = require('fs');
+const fs = require("fs");
+const path = require("path");
+const { parse } = require("csv-parse");
 
 const habitablePlanets = [];
 
@@ -7,31 +8,42 @@ const habitablePlanets = [];
 // koi_insol: amount of starlight planet gets
 // koi_prad: planetary radius
 function isHabitablePlanet(planet) {
-  return planet['koi_disposition'] === 'CONFIRMED'
-    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
-    && planet['koi_prad'] < 1.6;
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
 }
 
-fs.createReadStream('kepler_data.csv')
-  .pipe(parse({
-    comment: '#',
-    columns: true,
-  }))
-  .on('data', (data) => {
-    if (isHabitablePlanet(data)) {
-      habitablePlanets.push(data);
-    }
-  })
-  .on('error', (err) => {
-    console.log(err);
-  })
-  .on('end', () => {
-    console.log(habitablePlanets.map((planet) => {
-      return planet['kepler_name'];
-    }));
-    console.log(`${habitablePlanets.length} habitable planets found!`);
+async function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(
+      path.join(__dirname, "../..", "data", "kepler_data.csv")
+    )
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (data) => {
+        if (isHabitablePlanet(data)) {
+          habitablePlanets.push(data);
+        }
+      })
+      .on("error", (err) => {
+        console.log(err);
+        reject(err);
+      })
+      .on("end", () => {
+        console.log(`${habitablePlanets.length} habitable planets found!`);
+        resolve();
+      });
   });
+}
 
-  module.exports = {
-    planets: habitablePlanets
-  }
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets,
+};
--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,17 @@
 const http = require("http");
 const app = require("./app.js");
+const { loadPlanetsData } = require('./models/planets.model');
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
-
 const server = http.createServer(app);
 
-server.listen(PORT, () =>
-  console.log(`Server listening on localhost:${PORT}...`)
-);
+async function startServer() {
+  await loadPlanetsData();
+  
+  server.listen(PORT, () =>
+    console.log(`Server listening on localhost:${PORT}...`)
+  );
+}
+
+startServer();
