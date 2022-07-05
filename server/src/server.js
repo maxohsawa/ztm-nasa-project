6285febb7d3a0fca19b90001be20bda40148d043
Max Ohsawa <max.ohsawa@gmail.com>
2022-07-05 19:34:07 -0400

    adds mongoose, mongodb atlas connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,30 @@
 const http = require("http");
+const mongoose = require("mongoose");
 const app = require("./app.js");
-const { loadPlanetsData } = require('./models/planets.model');
+const { loadPlanetsData } = require("./models/planets.model");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_NAME = "nasa";
+const MONGODB_ATLAS_URL = `mongodb+srv://${DB_USER}:${DB_PASS}@nasacluster.i6sbf.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection open");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGODB_ATLAS_URL);
+
   await loadPlanetsData();
-  
+
   server.listen(PORT, () =>
     console.log(`Server listening on localhost:${PORT}...`)
   );
