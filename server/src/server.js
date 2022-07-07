05b114f4f1db1e092d952e7ad4fadf857d8ecd46
Max Ohsawa <max.ohsawa@gmail.com>
2022-07-07 16:58:53 -0400

    adds spacex api call to load launch data, but does not yet handle pagination

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ const http = require("http");
 const app = require("./app.js");
 const { mongoConnect } = require("./services/mongo");
 const { loadPlanetsData } = require("./models/planets.model");
+const { loadLaunchData } = require("./models/launches.model");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
@@ -11,6 +12,7 @@ const server = http.createServer(app);
 async function startServer() {
   await mongoConnect();
   await loadPlanetsData();
+  await loadLaunchData();
 
   server.listen(PORT, () =>
     console.log(`Server listening on localhost:${PORT}...`)
